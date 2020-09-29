426d98fba808aeeacc9e60ab793fe365fabdc0e1
opticks1704 <opticks1704@gmail.com>
2020-09-30 02:03:33 +0300

    new version

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -7,16 +7,15 @@
 //using namespace std;
 //inline void keep_window_open() { char ch; cin >> ch; }
 
-vector<string> acceptedDays = {
- "Monday", "monday", "mon", "Mon",
- "Tuesday", "tuesday", "tue", "Tue",
- "Wednesday", "wednesday", "wed", "Wed",
- "Thursday", "thursday", "thurs", "Thurs",
- "Friday", "friday", "fri", "Fri",
- "Saturday", "saturday", "sat", "Sat",
- "Sunday", "sunday", "sun", "Sun" };
-vector<string>dayys{ "Monday" ,"Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" };
-vector <vector<int>>days{ {},{},{},{},{},{},{} };//2d vector
+vector <vector<int>>days{ {},{},{},{},{},{},{} };//2d vector NUMBERS
+vector <vector<string>>daysname{ {"Monday", "monday", "mon", "Mon"},
+                                 {"Tuesday", "tuesday", "tue", "Tue"},
+                                 {"Wednesday", "wednesday", "wed", "Wed"},
+                                 {"Thursday", "thursday", "thurs", "Thurs"},
+                                 {"Friday", "friday", "fri", "Fri"},
+                                 { "Saturday", "saturday", "sat", "Sat"},
+                                 { "Sunday", "sunday", "sun", "Sun"} };//2d vector NAMES
+
 
 double sum(vector<int>s) {
     double result{};
@@ -27,31 +26,17 @@ double sum(vector<int>s) {
 
 void input(string s, int val) {
     while (cin >> s >> val) {
-        //cancer begins
-        if (s == acceptedDays[0] || s == acceptedDays[1] || s == acceptedDays[2] || s == acceptedDays[3])
-            days[0].push_back(val);
-        else if (s == acceptedDays[4] || s == acceptedDays[5] || s == acceptedDays[6] || s == acceptedDays[7])
-            days[1].push_back(val);
-        else if (s == acceptedDays[8] || s == acceptedDays[9] || s == acceptedDays[10] || s == acceptedDays[11])
-            days[2].push_back(val);
-        else if (s == acceptedDays[12] || s == acceptedDays[13] || s == acceptedDays[14] || s == acceptedDays[15])
-            days[3].push_back(val);
-        else if (s == acceptedDays[16] || s == acceptedDays[17] || s == acceptedDays[18] || s == acceptedDays[19])
-            days[4].push_back(val);
-        else if (s == acceptedDays[20] || s == acceptedDays[21] || s == acceptedDays[22] || s == acceptedDays[23])
-            days[5].push_back(val);
-        else if (s == acceptedDays[24] || s == acceptedDays[25] || s == acceptedDays[26] || s == acceptedDays[27])
-            days[6].push_back(val);
-        //finally!
-        else {
-            error("bad input");
+        for (int i{}; i < daysname.size(); ++i) {
+            for (int j{}; j < daysname[i].size(); ++j)
+                if (s == daysname[i][j]) days[i].push_back(val);
         }
+        
     }
 }
 
 void display() {
-    for (int i{}; i < dayys.size(); ++i) {
-        cout << '\n' << dayys[i] << " : ";
+    for (int i{}; i < daysname.size(); ++i) {
+        cout << '\n' << daysname[i][0] << " : ";
         if (!days[i].empty()) cout << sum(days[i]);
         else
             cout << "NOVALUE";
@@ -62,6 +47,7 @@ int main() {
     try {
         string s{};
         int val{};
+        //test();
         input(s, val);
         display();
         return 0;
